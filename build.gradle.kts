ebc0411c95a7a70080a0fc386cd647c39efc9c57
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-12-07 21:40:15 +0900

    build: Bump dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,14 @@ tasks.withType<JavaCompile> {
 allprojects {
     repositories {
         mavenCentral()
+        // Obtain baksmali/smali from source builds - https://github.com/iBotPeaches/smali
+        // Remove when official smali releases come out again.
+        maven {
+            url = uri("https://jitpack.io")
+            content {
+                includeGroup("com.github.iBotPeaches.smali")
+            }
+        }
         google()
     }
 }
